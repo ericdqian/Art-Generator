116b775b66a62c97f177323b03517071a3a05191
eric-qian-d <eric.qian.d@gmail.com>
2018-12-02 19:41:10 -0500

    New generator structure

--- main.py
+++ main.py
@@ -14,21 +14,20 @@ vae_params = {'conv1_output_size' : 32, 'conv1_kernel_size' : 3, 'stride1c' : 2,
 			  'conv3_output_size' : 128, 'conv3_kernel_size' : 3, 'stride3c' : 2, 
 			  'conv4_output_size' : 256, 'conv4_kernel_size' : 3, 'stride4c' : 2, 
 			  'conv5_output_size' : 512, 'conv5_kernel_size' : 3, 'stride5c' : 2, 
-			  'hidden1_size' : 1000, 'hidden2_size' : 200, 
+			  'hidden1_size' : 500, 'hidden2_size' : 200, 
 			  'deconv0_output_size' : 256, 'deconv1_kernel_size' : 3, 'stride1d' : 2,
 			  'deconv1_output_size' : 128, 'deconv1_kernel_size' : 3, 'stride1d' : 2, 
 			  'deconv2_output_size' : 64, 'deconv2_kernel_size' : 3, 'stride1d' : 2,
 			  'deconv3_output_size' : 32, 'deconv3_kernel_size' : 3, 'stride1d' : 2, 
 			  'deconv4_output_size' : 3, 'deconv4_kernel_size' : 3, 'stride2d' : 2, 
-
-			  'latent_vector_size': 100}
+			  'latent_vector_size': 400, 'd' : 128}
 data_load_params = {'batch_size' : 10, 'shuffle' : True, 'cuda': False, 'dataloader_workers' : 1, 'pin_memory' : True}
 
 data = Loader(data_load_params)
 model = VAE(vae_params)
 
 def train():
-	training_params = {'num_epochs' : 20, 'learning_rate' : 0.1, 'weight_decay' : 0.1, 'learning_rate_decay' : 0.9999, 'cuda' : False, 
+	training_params = {'num_epochs' : 2, 'learning_rate' : 0.1, 'weight_decay' : 0.3, 'learning_rate_decay' : 0.9999, 'cuda' : False, 
 		'summary_dir' : './runs/logs/', 'checkpoint_dir' : './runs/checkpoints/'}
 
 	loss = Loss()
@@ -41,24 +40,28 @@ def train():
 
 # CAUTION: Note that vae_params must be the same as the checkpoint... perhaps we can save this with the checkpoint for future
 def gen_image(checkpoint_file):
-	checkpoint = torch.load('./runs/checkpoints/checkpoint1.pth.tar')
+	checkpoint = torch.load('./runs/checkpoints/checkpoint2.pth.tar')
 	model.load_state_dict(checkpoint['state_dict'])
 
 	# print(data.train_set[0][0])
 	# print(data.train_set[0][0].size())
-	batch1 = data.train_set[1][0].unsqueeze(0)
+	batch1 = data.train_set[9][0].unsqueeze(0)
 	# print(batch1.size())
+
 	inp = Variable(batch1)
 	tvut.save_image(batch1, "goal2.png")
 	print(inp)
 	model.eval()
 	mu, logvar = model.encode(batch1)
 	lat = model.reparamaterize(mu, logvar)
-	print(mu)
-	tvut.save_image(model.decode(mu), "generated_image2.png")
+	
+	generated = model.decode(mu)
+
+	print(mu, logvar, generated)
+	tvut.save_image(generated, "generated_image2.png")
 	
 
 
 if __name__ == "__main__":
 	train()
-	gen_image('./runs/checkpoints/checkpoint1.pth.tar')
+	gen_image('./runs/checkpoints/checkpoint2.pth.tar')
--- vae.py
+++ vae.py
@@ -40,34 +40,56 @@ class VAE(nn.Module):
 			padding = self.params['conv5_kernel_size']//2, stride = 2)
 		self.bn5c = nn.BatchNorm2d(self.params['conv5_output_size'])
 
+		self.l1 = nn.Linear(4 * 4 * self.params['conv5_output_size'], 4 * 4 * self.params['conv5_output_size'])
+		self.l1bn = nn.BatchNorm1d(4 * 4 * self.params['conv5_output_size'])
+
 		self.efc1 = nn.Linear(4 * 4 * self.params['conv5_output_size'], self.params['latent_vector_size'])
 		self.efc1_bn = nn.BatchNorm1d(self.params['latent_vector_size'])
 		self.efc21 = nn.Linear(self.params['latent_vector_size'], self.params['latent_vector_size'])
 		self.efc22 = nn.Linear(self.params['latent_vector_size'], self.params['latent_vector_size'])
 
 		#decoder 
-		self.dfc1 = nn.Linear(self.params['latent_vector_size'], self.params['hidden1_size'])
-		self.dfc1_bn = nn.BatchNorm1d(self.params['hidden1_size'])
-		self.dfc2 = nn.Linear(self.params['hidden1_size'], 4 * 4 * self.params['conv5_output_size'])
-		self.dfc2_bn = nn.BatchNorm1d(4 * 4 * self.params['conv5_output_size'])
+		# self.dfc1 = nn.Linear(self.params['latent_vector_size'], self.params['hidden1_size'])
+		# self.dfc1_bn = nn.BatchNorm1d(self.params['hidden1_size'])
+		# self.dfc2 = nn.Linear(self.params['hidden1_size'], 4 * 4 * self.params['conv5_output_size'])
+		# self.dfc2_bn = nn.BatchNorm1d(4 * 4 * self.params['conv5_output_size'])
 
-		self.deconv0 = nn.ConvTranspose2d(self.params['conv5_output_size'], self.params['conv4_output_size'], kernel_size = self.params['deconv1_kernel_size'],
-			padding = 1, output_padding = 1, stride = 2)
-		self.bn0d = nn.BatchNorm2d(self.params['conv4_output_size'])
-		self.deconv1 = nn.ConvTranspose2d(self.params['conv4_output_size'], self.params['conv3_output_size'], kernel_size = self.params['deconv1_kernel_size'],
-			padding = 1, output_padding = 1, stride = 2)
-		self.bn1d = nn.BatchNorm2d(self.params['conv3_output_size'])
-		self.deconv2 = nn.ConvTranspose2d(self.params['conv3_output_size'], self.params['conv2_output_size'], kernel_size = self.params['deconv2_kernel_size'],
-			padding = 1, output_padding = 1, stride = 2)
-		self.bn2d = nn.BatchNorm2d(self.params['conv2_output_size'])
-		self.deconv3 = nn.ConvTranspose2d(self.params['conv2_output_size'], self.params['conv1_output_size'], kernel_size = self.params['deconv3_kernel_size'],
-			padding = 1, output_padding = 1, stride = 2)
-		self.bn3d = nn.BatchNorm2d(self.params['conv1_output_size'])
+		# self.deconv0 = nn.ConvTranspose2d(self.params['conv5_output_size'], self.params['conv4_output_size'], kernel_size = self.params['deconv1_kernel_size'],
+		# 	padding = 1, output_padding = 1, stride = 2)
+		# self.bn0d = nn.BatchNorm2d(self.params['conv4_output_size'])
+		# self.deconv1 = nn.ConvTranspose2d(self.params['conv4_output_size'], self.params['conv3_output_size'], kernel_size = self.params['deconv1_kernel_size'],
+		# 	padding = 1, output_padding = 1, stride = 2)
+		# self.bn1d = nn.BatchNorm2d(self.params['conv3_output_size'])
+		# self.deconv2 = nn.ConvTranspose2d(self.params['conv3_output_size'], self.params['conv2_output_size'], kernel_size = self.params['deconv2_kernel_size'],
+		# 	padding = 1, output_padding = 1, stride = 2)
+		# self.bn2d = nn.BatchNorm2d(self.params['conv2_output_size'])
+		# self.deconv3 = nn.ConvTranspose2d(self.params['conv2_output_size'], self.params['conv1_output_size'], kernel_size = self.params['deconv3_kernel_size'],
+		# 	padding = 1, output_padding = 1, stride = 2)
+		# self.bn3d = nn.BatchNorm2d(self.params['conv1_output_size'])
 
-		self.deconv4 = nn.ConvTranspose2d(self.params['conv1_output_size'], 3, kernel_size = self.params['deconv4_kernel_size'], 
-			padding = 1, output_padding = 1, stride = 2)
+		# self.deconv4 = nn.ConvTranspose2d(self.params['conv1_output_size'], 3, kernel_size = self.params['deconv4_kernel_size'], 
+		# 	padding = 1, output_padding = 1, stride = 2)
 
-		self.relu = nn.ReLU(0.1)
+		self.deconv_linear = nn.Linear(self.params['latent_vector_size'], self.params['d'] * 16 * 4 * 4)
+		self.deconv_linear_bn = nn.BatchNorm1d(self.params['d'] * 16 * 4 * 4)
+		# self.deconv1 = nn.ConvTranspose2d(self.params['latent_vector_size'], 16 * self.params['d'], kernel_size = 4,
+		# 	padding = 1, output_padding = 1, stride = 2)
+		self.deconv2 = nn.ConvTranspose2d(16 * self.params ['d'], 8 * self.params['d'], kernel_size = 4,
+			padding = 1, output_padding = 0, stride = 2)
+		self.deconv2_bn = nn.BatchNorm2d(self.params['d'] * 8)
+		self.deconv3 = nn.ConvTranspose2d(8 * self.params['d'], 4 * self.params['d'], kernel_size = 4,
+			padding = 1, output_padding = 0, stride = 2)
+		self.deconv3_bn = nn.BatchNorm2d(self.params['d'] * 4)
+		self.deconv4 = nn.ConvTranspose2d(4 * self.params['d'], 2 * self.params['d'], kernel_size = 4,
+			padding = 1, output_padding = 0, stride = 2)
+		self.deconv4_bn = nn.BatchNorm2d(self.params['d'] * 2)
+		self.deconv5 = nn.ConvTranspose2d(2 * self.params['d'], self.params['d'], kernel_size = 4,
+			padding = 1, output_padding = 0, stride = 2)
+		self.deconv5_bn = nn.BatchNorm2d(self.params['d'])
+		self.deconv6 = nn.ConvTranspose2d(self.params['d'], 3, kernel_size = 4,
+			padding = 1, output_padding = 0, stride = 2)
+
+		self.relu = nn.LeakyReLU(0.05)
 
 	def encode(self, x):
 		x = self.relu(self.bn1c(self.conv1(x)))
@@ -75,7 +97,9 @@ class VAE(nn.Module):
 		x = self.relu(self.bn3c(self.conv3(x)))
 		x = self.relu(self.bn4c(self.conv4(x)))
 		x = self.relu(self.bn5c(self.conv5(x))).view(-1, 4 * 4 * self.params['conv5_output_size'])
+		# x = self.relu(self.l1bn(self.l1(x)))
 		x = self.relu(self.efc1_bn(self.efc1(x)))
+		# print(x.size())
 		return self.efc21(x), self.efc22(x)
 
 	def reparamaterize(self, mu, logvar):
@@ -87,14 +111,31 @@ class VAE(nn.Module):
 			return mu
 
 	def decode(self, z):
-		z = self.relu(self.dfc1_bn(self.dfc1(z)))
-		z = self.relu(self.dfc2_bn(self.dfc2(z))).view(-1, self.params['conv5_output_size'], 4, 4)
-		z = self.relu(self.bn0d(self.deconv0(z)))
-		z = self.relu(self.bn1d(self.deconv1(z)))
-		z = self.relu(self.bn2d(self.deconv2(z)))
-		z = self.relu(self.bn3d(self.deconv3(z)))
+		# z = self.relu(self.dfc1_bn(self.dfc1(z)))
+		# z = self.relu(self.dfc2_bn(self.dfc2(z))).view(-1, self.params['conv5_output_size'], 4, 4)
+		# z = self.relu(self.bn0d(self.deconv0(z)))
+		# z = self.relu(self.bn1d(self.deconv1(z)))
+		# z = self.relu(self.bn2d(self.deconv2(z)))
+		# z = self.relu(self.bn3d(self.deconv3(z)))
 
-		z = self.relu(self.deconv4(z))
+		# z = self.relu(self.deconv4(z))
+		# z = z.view(-1, 3, 128, 128)
+		# return z
+
+		# z = z.view(-1, self.params['latent_vector_size'], 4, 4)
+		# print(z.size())
+		z = self.relu(self.deconv_linear_bn(self.deconv_linear(z)).view(-1, self.params['d'] * 16, 4, 4))
+		# z = self.relu(self.deconv1(z))
+		# print(z.size())
+		z = self.relu(self.deconv2_bn(self.deconv2(z)))
+		# print(z.size())
+		z = self.relu(self.deconv3_bn(self.deconv3(z)))
+		# print(z.size())
+		z = self.relu(self.deconv4_bn(self.deconv4(z)))
+		# print(z.size())
+		z = self.relu(self.deconv5_bn(self.deconv5(z)))
+		# print(z.size())
+		z = self.relu(self.deconv6(z))
 		z = z.view(-1, 3, 128, 128)
 		return z
 
