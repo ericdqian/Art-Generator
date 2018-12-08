321e7f6f95fbff4934c9be0eb030f9502009a5e2
albert-yue <albert.s.yue@gmail.com>
2018-12-08 11:12:20 -0500

    added top-k accuracy

--- rasta/rasta.py
+++ rasta/rasta.py
@@ -8,6 +8,9 @@ import torch.optim as optim
 from torch.autograd import Variable
 from tensorboardX import SummaryWriter
 
+from PIL import Image
+Image.MAX_IMAGE_PIXELS = 1000000000
+
 from data import WikiArtDataLoader, get_classes
 
 def train_model(model, dataloader, criterion, optimizer, scheduler, use_gpu=False, num_epochs=25, log_dir="runs/logs/cnn"):
@@ -42,14 +45,12 @@ def train_model(model, dataloader, criterion, optimizer, scheduler, use_gpu=Fals
                 model.eval()   # Set model to evaluate mode
 
             running_loss = 0.0
-            running_corrects = 0
+            running_corrects_1 = 0
+            running_corrects_3 = 0
+            running_corrects_5 = 0
 
             # Iterate over data
-            i = 0
             for inputs, labels in dataloader[phase]:
-                if i % 100 == 0:
-                    print(i)
-                i+=1
 
                 if use_gpu:
                     inputs = Variable(inputs.cuda())
@@ -62,8 +63,9 @@ def train_model(model, dataloader, criterion, optimizer, scheduler, use_gpu=Fals
                 # track history if only in train
                 with torch.set_grad_enabled(phase == 'train'):
                     outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
                     loss = criterion(outputs, labels)
+
+                    acc1, acc3, acc5 = accuracy(outputs, labels, topk=(1,3, 5))
 
                     # backward + optimize only if in training phase
                     if phase == 'train':
@@ -72,16 +74,22 @@ def train_model(model, dataloader, criterion, optimizer, scheduler, use_gpu=Fals
 
                 # statistics
                 running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
+                running_corrects_1 += acc1
+                running_corrects_3 += acc3
+                running_corrects_5 += acc5
 
             epoch_loss = running_loss / len(dataloader[phase])
-            epoch_acc = running_corrects.double() / len(dataloader[phase])
+            epoch_acc_1 = running_corrects_1.double() / len(dataloader[phase])
+            epoch_acc_3 = running_corrects_3.double() / len(dataloader[phase])
+            epoch_acc_5 = running_corrects_5.double() / len(dataloader[phase])
 
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
+            print('{} Loss: {:.4f} Acc@1: {:.4f} Acc@3: {:.4f} Acc@5: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc_1, epoch_acc_3, epoch_acc_5))
 
             writer.add_scalar('training/{}/loss'.format(phase), epoch_loss, epoch+1)
-            writer.add_scalar('training/{}/acc'.format(phase), epoch_acc, epoch+1)
+            writer.add_scalar('training/{}/acc@1'.format(phase), epoch_acc_1, epoch+1)
+            writer.add_scalar('training/{}/acc@3'.format(phase), epoch_acc_3, epoch+1)
+            writer.add_scalar('training/{}/acc@5'.format(phase), epoch_acc_5, epoch+1)
             save_checkpoint({
                 'epoch': epoch + 1,
                 'state_dict': model.state_dict(),
@@ -105,6 +113,25 @@ def train_model(model, dataloader, criterion, optimizer, scheduler, use_gpu=Fals
     model.load_state_dict(best_model_wts)
     return model
 
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k
+    
+    Taken from https://github.com/pytorch/examples/blob/master/imagenet/main.py
+    """
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
 def save_checkpoint(state, is_best=False, checkpoint_dir='runs/checkpoints/', filename='checkpoint.pth.tar'):
     '''
     a function to save checkpoint of the training
@@ -127,7 +154,17 @@ def create_dir(directory):
 
 if __name__ == '__main__':
     data_path = 'data/wikiart'
-    wikiart_loader = WikiArtDataLoader(data_path, 9, (0.8, 0.1, 0.1), random_seed=42, num_workers=4, pin_memory=False)
+
+    use_gpu = False
+    num_workers = 4
+    pin_memory = False
+    if torch.cuda.is_available():
+        use_gpu = True
+        num_workers = 1
+        pin_memory = True
+        print('Using GPU')
+
+    wikiart_loader = WikiArtDataLoader(data_path, 32, (0.8, 0.1, 0.1), random_seed=42, num_workers=num_workers, pin_memory=pin_memory)
 
     model = models.resnet50(pretrained=True)
 
@@ -140,11 +177,8 @@ if __name__ == '__main__':
     num_styles = len(get_classes(data_path))
     model.fc = nn.Linear(num_ftrs, num_styles)
 
-    use_gpu = False
-    if torch.cuda.is_available():
-        use_gpu = True
+    if use_gpu:
         model = model.cuda()
-        print('Using GPU')
 
     criterion = nn.CrossEntropyLoss()
 
@@ -152,7 +186,7 @@ if __name__ == '__main__':
 
     exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size=5, gamma=0.1)
 
-    model = train_model(model, wikiart_loader, criterion, optimizer_ft, exp_lr_scheduler, use_gpu=use_gpu, num_epochs=50)
+    model = train_model(model, wikiart_loader, criterion, optimizer_ft, exp_lr_scheduler, use_gpu=use_gpu, num_epochs=25)
     create_dir('runs/models/') 
     torch.save({
         'state_dict': model.state_dict(),
