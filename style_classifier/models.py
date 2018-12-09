80d28c185e8ea685b155c6b00bd4c4dbf247d32a
albert-yue <albert.s.yue@gmail.com>
2018-12-09 11:56:24 -0500

    fix where_retrain

--- style_classifier/models.py
+++ style_classifier/models.py
@@ -7,18 +7,30 @@ import torchvision.models as models
 
 from data import get_classes
 
-def get_model(model_type, data_path, percentage_retrain=0.0, pretrained=True):
+def get_model(model_type, data_path, percentage_retrain=0.0, where="end", pretrained=True):
     if model_type == "resnet50":
         model = models.resnet50(pretrained=True)
 
     # freeze model params
-    _retrain_percent(model, percentage_retrain)
+    _retrain_percent(model, percentage_retrain, where)
 
     # replace output layer
     num_ftrs = model.fc.in_features
     num_styles = len(get_classes(data_path))
     model.fc = nn.Linear(num_ftrs, num_styles)
     return model
+
+def _retrain_percent(model, percentage, where):
+    """
+    Args:
+        percentage (int): percent of layers (excluding output) to retrain from the output layer
+    """
+    total_layers = len(list(model.parameters()))
+    num_retrain = math.floor(percentage * total_layers)
+    if where == "central":
+        _retrain_central_n(model, num_retrain, total_layers)
+    else:
+        _retrain_n(model, num_retrain, total_layers)
 
 def _freeze_starting_n(model, num_freeze):
     """
@@ -38,18 +50,6 @@ def _retrain_n(model, num_retrain, total_layers=None):
     if total_layers is None:
         total_layers = len(list(model.parameters()))
     _freeze_starting_n(model, total_layers - num_retrain - 1)
-
-def _retrain_percent(model, percentage, where):
-    """
-    Args:
-        percentage (int): percent of layers (excluding output) to retrain from the output layer
-    """
-    total_layers = len(list(model.parameters()))
-    num_retrain = math.floor(percentage * total_layers)
-    if where == "central":
-        _retrain_central_n(model, num_retrain, total_layers)
-    else:
-        _retrain_n(model, num_retrain, total_layers)
 
 def _retrain_central_n(model, num_retrain, total_layers=None):
     """
